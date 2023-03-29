567a497b77448744aef871285094a7b4801e80b8
Nadia Lavagnino <nadia.lavagninoalejandra@gmail.com>
2023-03-28 21:19:12 -0300

    Inicio de proyecto

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>react2</h3>
+            <div>
+                <button>Remeras</button>
+                <button>Jean</button>
+                <button>Camperas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
